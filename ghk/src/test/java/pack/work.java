a52bf49a10e1b80c294938d4a0f2d699244a8577
LENOVO <LENOVO@DESKTOP-6F1UH3N>
2022-11-15 15:03:19 +0530

    this is total new

---
+++ ghk/src/test/java/pack/work.java
@@ -0,0 +1,11 @@
+package pack;
+
+public class work {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("class1");
+		System.out.println("add");
+		
+	}
+}
---
+++ ghk/src/test/java/pack/work2.java
@@ -0,0 +1,13 @@
+package pack;
+
+public class work2 {
+	
+	public static void main(String[]args)
+	{
+		System.out.println("my self kartik");
+		System.out.println("I live in amravati");
+		System.out.println("I am working in ddt company");
+		System.out.println("add23");
+	}
+
+}
