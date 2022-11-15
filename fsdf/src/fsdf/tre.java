00ec66a954d19b862f4197ef22ae0e395108efe7
LENOVO <LENOVO@DESKTOP-6F1UH3N>
2022-11-15 17:42:26 +0530

    this is

---
+++ fsdf/src/fsdf/tre.java
@@ -0,0 +1,10 @@
+package fsdf;
+
+public class tre {
+	
+	public static void main(String[] args)
+	{
+             System.out.println("enter something");
+	}
+
+}
--- fsdf/src/fsdf/xvdfgd.java
+++ fsdf/src/fsdf/xvdfgd.java
@@ -2,5 +2,11 @@ package fsdf;
 
 public class xvdfgd 
 {
+	public static void main(String[] args) 
+{
+		
    System.out.println("something");
+   
+}
+   
 }
