63b43e10426f86e4b0aadbad9067889ac7379020
KarNanda123 <118243349+KarNanda123@users.noreply.github.com>
2022-11-15 17:46:10 +0530

    Update xvdfgd.java

--- fsdf/src/fsdf/xvdfgd.java
+++ fsdf/src/fsdf/xvdfgd.java
@@ -2,10 +2,9 @@ package fsdf;
 
 public class xvdfgd 
 {
-	
-		
-   System.out.println("something");
-   
-
+	public static void main (String[]args)
+{		
+              System.out.println("something");
+}
    
 }
